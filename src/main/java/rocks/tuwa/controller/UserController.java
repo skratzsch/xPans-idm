4253c483f140dee3551c1fca771434a1bea752cd
skratzsch <skratzsch@yahoo.de>
2023-12-27 22:22:07 +0100

    initial UserController

--- src/main/java/rocks/tuwa/controller/UserController.java
+++ src/main/java/rocks/tuwa/controller/UserController.java
@@ -1,4 +1,26 @@
 package rocks.tuwa.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import rocks.tuwa.services.UserService;
+
+@RestController
+@RequestMapping("/xpans")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/validateUser")
+    public ResponseEntity<?> validateUser(@RequestParam String userId,
+                                          @RequestParam String password) {
+        // magic
+        return ResponseEntity.ok().build();
+    }
 }

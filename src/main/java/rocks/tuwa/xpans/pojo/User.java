433a90beba1f3463eb395ae8a1c7826f71448d98
skratzsch <skratzsch@yahoo.de>
2023-12-27 23:25:30 +0100

    initial UserDto

--- src/main/java/rocks/tuwa/xpans/pojo/User.java
+++ src/main/java/rocks/tuwa/xpans/pojo/User.java
@@ -1,4 +1,37 @@
 package rocks.tuwa.xpans.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Hex;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+@Getter
+@Setter
 public class User {
+
+    private String userId;
+    private String hashedPassword;
+
+    private User(String userId, String hashedPassword) {
+        this.userId = userId;
+        this.hashedPassword = hashedPassword;
+    }
+
+    public static User fromUser(UserDto userDto) {
+        return new User(userDto.getUserId(), User.hashPassword(userDto.getPassword()));
+    }
+
+    private static String hashPassword(String password) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256", new BouncyCastleProvider());
+            byte[] hash = digest.digest(password.getBytes(StandardCharsets.UTF_8));
+            return new String(Hex.encode(hash));
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException("Fehler beim Hashen des Passworts", e);
+        }
+    }
 }
--- src/main/java/rocks/tuwa/xpans/pojo/UserDto.java
+++ src/main/java/rocks/tuwa/xpans/pojo/UserDto.java
@@ -1,4 +1,11 @@
 package rocks.tuwa.xpans.pojo;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserDto {
+    private String userId;
+    private String password;
 }
